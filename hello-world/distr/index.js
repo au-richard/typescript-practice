083e352f1bffb2236b748861de5527423745c9f2
Richard Au <au.richard4@gmail.com>
2022-09-26 14:47:21 -0700

    Testing declaring different types of variables.

--- hello-world/distr/index.js
+++ hello-world/distr/index.js
@@ -4,4 +4,8 @@ if (age < 50) {
     age += 10;
 }
 console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -3,3 +3,10 @@ if (age < 50) {
   age += 10;
 }
 console.log(age);
+
+
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
