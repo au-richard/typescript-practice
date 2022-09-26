269dbdfc18f57f9675785c4183cda85997f128b2
Richard Au <au.richard4@gmail.com>
2022-09-26 15:13:52 -0700

    Testing Tuples and Enums.

--- hello-world/distr/index.js
+++ hello-world/distr/index.js
@@ -8,4 +8,16 @@ let sales = 123456789;
 let course = "TypeScript";
 let is_published = true;
 let level;
+let numArray = [];
+let anyArray = [];
+let user = [1, "Richard"];
+var Size;
+(function (Size) {
+    Size["Small"] = "s";
+    Size["Medium"] = "m";
+    Size["Large"] = "l";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,12 +1,30 @@
+//Testing debugging
 let age: number = 20;
 if (age < 50) {
   age += 10;
 }
 console.log(age);
 
-
+// Trying out different var declarations
 let sales = 123_456_789;
 let course = "TypeScript";
 let is_published = true;
 let level;
 
+// Testing types with arrays
+let numArray: number[] = [];
+let anyArray = [];
+
+// Tuple Arrays
+let user: [number, string] = [1, "Richard"];
+
+// Enums
+// JavaScript way of labelling constants
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// //PascaleCase
+enum Size { Small = "s", Medium = "m", Large = "l" };
+let mySize: Size = Size.Medium;
+console.log(mySize);
