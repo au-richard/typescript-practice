5a77f3c5cbe4e2823db926763fe7d0612bd939ab
Richard Au <au.richard4@gmail.com>
2022-09-27 15:09:19 -0700

    Finishing calculating Last Index in typescript.

--- hello-world/distr/index.js
+++ hello-world/distr/index.js
@@ -47,4 +47,18 @@ const ageCalculator = function (name, yearOfBirth, currentYear) {
 console.log(ageCalculator("Suzie", 1984, 2016));
 console.log(ageCalculator("Jack", 2004, 2016));
 console.log(ageCalculator("Ali", 2015, 2015));
+const lastIndexOf = function (numArr, num) {
+    for (let i = numArr.length - 1; i >= -1; i--) {
+        if (numArr[i] === num) {
+            return i;
+        }
+    }
+    return -1;
+};
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+console.log(lastIndexOf([3], 3), "=?", 0);
 //# sourceMappingURL=index.js.map
