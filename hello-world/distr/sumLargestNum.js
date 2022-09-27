affe3221a9b22098139d10d4ba23d7b873b3a90a
Richard Au <au.richard4@gmail.com>
2022-09-27 16:20:16 -0700

    Finishing calculating sum of largest numbers in array.

---
+++ hello-world/distr/sumLargestNum.js
@@ -0,0 +1,18 @@
+"use strict";
+const sumLargestNum = function (data) {
+    let num = data[0];
+    if (data.length < 2) {
+        return "Array length must be at least 2";
+    }
+    else {
+        for (let i = 1; i < data.length; i++) {
+            num += data[i];
+        }
+        return `The answer is ${num}.`;
+    }
+};
+console.log(sumLargestNum([10]));
+console.log(sumLargestNum([1, 10]));
+console.log(sumLargestNum([1, 2, 3]));
+console.log(sumLargestNum([10, 4, 34, 6, 92, 2]));
+//# sourceMappingURL=sumLargestNum.js.map
