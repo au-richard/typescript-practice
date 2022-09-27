7ba60fb99a5012381d90bf754c69f69192dae533
Richard Au <au.richard4@gmail.com>
2022-09-27 15:53:32 -0700

    Starting smart garbage task.

---
+++ hello-world/src/smartGarbage.ts
@@ -0,0 +1,12 @@
+let bins: {
+  waste: number,
+  recycling: number,
+  compost: number;
+} = {
+  waste: 0,
+  recycling: 0,
+  compost: 0
+};
+const smartGarbage = function (trash: string, bins: object) {
+  if (trash === Object.keys(bins))
+};
