424b4ad027638d76485c07bde62a25f27e0bef50
Richard Au <au.richard4@gmail.com>
2022-09-28 16:23:22 -0700

    Finishing percent encoded string.

--- hello-world/src/percentEncodedString.ts
+++ hello-world/src/percentEncodedString.ts
@@ -2,4 +2,12 @@ const urlEncode = function (text: string): string {
   let newString = "";
   text = text.trim();
 
+  for (let i = 0; i < text.length; i++) {
+    text[i] === " " ? newString += "%20" : newString += text[i];
+  }
+  return newString;
 };
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
