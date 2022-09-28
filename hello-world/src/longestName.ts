f1428ae2985084496e9bae7a15549d1848701331
Richard Au <au.richard4@gmail.com>
2022-09-28 13:42:19 -0700

    Adding console log inputs.

--- hello-world/src/longestName.ts
+++ hello-world/src/longestName.ts
@@ -1,3 +1,20 @@
 const instructorWithLongestName = function (instructors: object[]): object {
-
+  let currentName = instructors[0].name;
+  for (let i = 1; i < instructors.length; i++) {
+    if (instructors[i].name.length > instructors[i - 1].name.length) {
+      result.name = instructors[i].name;
+    }
+  }
 };
+
+console.log(instructorWithLongestName([
+  { name: "Samuel", course: "iOS" },
+  { name: "Jeremiah", course: "Web" },
+  { name: "Ophilia", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+console.log(instructorWithLongestName([
+  { name: "Matthew", course: "Web" },
+  { name: "David", course: "iOS" },
+  { name: "Domascus", course: "Web" }
+]));
