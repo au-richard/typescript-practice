18f9c0b5c07f1fec4c7e88d5a9e4a51f1e716c2b
Richard Au <au.richard4@gmail.com>
2022-09-29 13:53:38 -0700

    Creating double for loop for checking row and columns in array.

--- hello-world/src/smartParking.ts
+++ hello-world/src/smartParking.ts
@@ -1,3 +1,8 @@
-// const whereCanIPark = function (spots, vehicle) {
+// const whereCanIPark = function (spots: string[], vehicle: string): array {
+//   let openSpot = false;
+//   for (let r = 0; r < spots.length; r++) {
+//     for (let c = 0; c < spots[r].length; c++) {
 
+//     }
+//   }
 // };
