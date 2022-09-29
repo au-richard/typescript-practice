31eb9d77a65cbb320bd3fdd0f31eb7af68bb4bd4
Richard Au <au.richard4@gmail.com>
2022-09-29 13:58:13 -0700

    Checking for regular vehicle parking spot.

--- hello-world/distr/smartParking.js
+++ hello-world/distr/smartParking.js
@@ -1,2 +1,13 @@
 "use strict";
+const whereCanIPark = function (spots, vehicle) {
+    let openSpot = [0, 0];
+    for (let r = 0; r < spots.length; r++) {
+        for (let c = 0; c < spots[r].length; c++) {
+            if (vehicle === "regular" && spots[r][c] === "R") {
+                openSpot = [c, r];
+            }
+        }
+    }
+    return openSpot;
+};
 //# sourceMappingURL=smartParking.js.map
