e9e6fa16e12ae7a47990d66ca95cc94b5f7f46b4
Richard Au <au.richard4@gmail.com>
2022-10-01 09:31:42 -0700

    Adding console logs for test case scenarios.

--- hello-world/distr/longestName.js
+++ hello-world/distr/longestName.js
@@ -8,4 +8,15 @@ const instructorWithLongestName = function (instructors) {
     }
     return currentName;
 };
+console.log(instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Jeremiah", course: "Web" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" }
+]));
+console.log(instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" }
+]));
 //# sourceMappingURL=longestName.js.map
