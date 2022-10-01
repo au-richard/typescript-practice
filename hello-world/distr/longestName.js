c322fd7e29f7e004209d710e8d6c246a57a24f7c
Richard Au <richardau@pop-os.localdomain>
2022-10-01 09:27:05 -0700

    Finishing longest name.

--- hello-world/distr/longestName.js
+++ hello-world/distr/longestName.js
@@ -1,2 +1,11 @@
 "use strict";
+const instructorWithLongestName = function (instructors) {
+    let currentName = instructors[0].name;
+    for (let i = 1; i < instructors.length; i++) {
+        if (instructors[i].name.length > currentName.length) {
+            currentName = instructors[i].name;
+        }
+    }
+    return currentName;
+};
 //# sourceMappingURL=longestName.js.map
