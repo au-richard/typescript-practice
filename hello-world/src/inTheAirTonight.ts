880847cef8523720af775f8ad076ce2719ed7d3a
Richard Au <au.richard4@gmail.com>
2022-10-02 14:33:05 -0700

    Checking if counter amount is greater or less than threshold.

--- hello-world/src/inTheAirTonight.ts
+++ hello-world/src/inTheAirTonight.ts
@@ -1,3 +1,14 @@
-// const checkAir = function (samples: string[], threshold: number): string {
+const checkAir = function (samples: string[], threshold: number): string {
+  let counter = 0;
+  for (let i = 0; i < samples.length; i++) {
+    if (samples[i] === "dirty") {
+      counter++;
+    }
+  }
+  if ((counter % samples.length) > (threshold *= 10)) {
+    return "Polluted";
+  } else {
+    return "Clean";
+  }
+};
 
-// }
