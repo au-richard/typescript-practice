98d2ed5d611b95414301e911a67affe2c69dd9a4
Richard Au <au.richard4@gmail.com>
2022-10-02 14:34:46 -0700

    Adding console logs for testing scenarios.

--- hello-world/src/inTheAirTonight.ts
+++ hello-world/src/inTheAirTonight.ts
@@ -12,3 +12,14 @@ const checkAir = function (samples: string[], threshold: number): string {
   }
 };
 
+console.log(checkAir(['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+));
