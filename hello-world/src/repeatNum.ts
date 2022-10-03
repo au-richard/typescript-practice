8024e96b43ce75aae35a136539e4258d0a7817de
Richard Au <au.richard4@gmail.com>
2022-10-03 15:02:37 -0700

    Finishing Repeating Numbers.

---
+++ hello-world/src/repeatNum.ts
@@ -0,0 +1,14 @@
+const repeatNum = function (data: number[][]): string {
+  let result = [];
+  let string = "";
+  for (let i = 0; i < data.length; i++) {
+    let num1 = data[i][0];
+    let num2 = data[i][1];
+    for (let h = 0; h < num2; h++) {
+      string += num1;
+    }
+    result.push(string);
+    string = "";
+  }
+  return result.join(", ");
+}; 
