718d47ae429c7e9c396d4caa9c4e4def182c8ac3
Richard Au <au.richard4@gmail.com>
2022-10-03 15:02:51 -0700

    Finishing Repeating Numbers.

--- hello-world/distr/repeatNum.js
+++ hello-world/distr/repeatNum.js
@@ -1,2 +1,16 @@
 "use strict";
+const repeatNum = function (data) {
+    let result = [];
+    let string = "";
+    for (let i = 0; i < data.length; i++) {
+        let num1 = data[i][0];
+        let num2 = data[i][1];
+        for (let h = 0; h < num2; h++) {
+            string += num1;
+        }
+        result.push(string);
+        string = "";
+    }
+    return result.join(", ");
+};
 //# sourceMappingURL=repeatNum.js.map
