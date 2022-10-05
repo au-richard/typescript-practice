7705dbbde5b820a46b8b576b56b5951b721b7900
Richard Au <au.richard4@gmail.com>
2022-10-05 14:29:32 -0700

    Finishing multiplication table.

---
+++ hello-world/distr/multiplyTable.js
@@ -0,0 +1,14 @@
+"use strict";
+const multiplicationTable = (maxValue) => {
+    let num = 0;
+    let result = "";
+    for (let i = 1; i <= maxValue; i++) {
+        for (let h = 1; h <= maxValue; h++) {
+            num = i * h;
+            result += num + " ";
+        }
+        result += "\n";
+    }
+    return result;
+};
+//# sourceMappingURL=multiplyTable.js.map
