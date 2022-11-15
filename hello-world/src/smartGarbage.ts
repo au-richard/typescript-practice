f731461ce42c7070a50e7c353c1095eb525992f4
Richard Au <au.richard4@gmail.com>
2022-11-15 15:31:17 -0800

    Testing different way of coding the problem.

--- hello-world/src/smartGarbage.ts
+++ hello-world/src/smartGarbage.ts
@@ -1,12 +1,23 @@
-let bins: {
-  waste: number,
-  recycling: number,
-  compost: number;
-} = {
+// let bins: {
+//   waste: number,
+//   recycling: number,
+//   compost: number;
+// } = {
+//   waste: 0,
+//   recycling: 0,
+//   compost: 0
+// };
+// const smartGarbage = function (trash: string, bins: object) {
+//   if (trash === Object.keys(bins))
+// };
+
+let bins = {
   waste: 0,
   recycling: 0,
   compost: 0
 };
-// const smartGarbage = function (trash: string, bins: object) {
-//   if (trash === Object.keys(bins))
-// };
+const smartGarbage = function (trash, bins) {
+  bins[trash]++;
+  return bins;
+};
+console.log(smartGarbage("recycling", { waste: 4, recycling: 2, compost: 5 }));
