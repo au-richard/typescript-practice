ed44b85fb2190907499b1c1562c0fce774d9bbcc
Richard Au <au.richard4@gmail.com>
2022-12-15 18:46:43 -0800

    Commenting out smartGarbage function.

--- hello-world/src/smartGarbage.ts
+++ hello-world/src/smartGarbage.ts
@@ -16,8 +16,8 @@ let bins = {
   recycling: 0,
   compost: 0
 };
-const smartGarbage = function (trash: number, bins: number) {
-  // bins[trash]++;
-  return bins;
-};
+// const smartGarbage = function (trash: number, bins: number) {
+//   // bins[trash]++;
+//   return bins;
+// };
 // console.log(smartGarbage("recycling", { waste: 4, recycling: 2, compost: 5 }));
