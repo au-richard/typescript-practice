a847947264265850470139324548631120a3fad7
Richard Au <au.richard4@gmail.com>
2022-12-15 18:45:13 -0800

    Fixing console log.

--- hello-world/src/smartGarbage.ts
+++ hello-world/src/smartGarbage.ts
@@ -20,4 +20,4 @@ const smartGarbage = function (trash: number, bins: number) {
   bins[trash]++;
   return bins;
 };
-console.log(smartGarbage("recycling", { waste: 4, recycling: 2, compost: 5 }));
+console.log(smartGarbage("recycling", { waste: 4, recycling: 2, compost: 5 })); 
