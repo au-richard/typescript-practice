68572cc7b59176726a1f509df238f9bf9bff2cf0
Richard Au <au.richard4@gmail.com>
2022-12-17 12:01:34 -0800

    Fixing code.

--- hello-world/distr/vowels.js
+++ hello-world/distr/vowels.js
@@ -1,16 +1,27 @@
 "use strict";
 const numVowels = function (data) {
-    let num = 0;
-    const newArr = data.split("");
-    for (let i = 0; i < newArr.length; i++) {
-        if (newArr[i] === "a" || newArr[i] === "e" || newArr[i] === "i" || newArr[i] === "o" || newArr[i] === "u") {
-            num += 1;
-        }
-        else if (newArr[i] === "A" || newArr[i] === "E" || newArr[i] === "I" || newArr[i] === "O" || newArr[i] === "U") {
-            num += 1;
-        }
+  let num = 0;
+  const newArr = data.split("");
+  for (let i = 0; i < newArr.length; i++) {
+    if (
+      newArr[i] === "a" ||
+      newArr[i] === "e" ||
+      newArr[i] === "i" ||
+      newArr[i] === "o" ||
+      newArr[i] === "u"
+    ) {
+      num += 1;
+    } else if (
+      newArr[i] === "A" ||
+      newArr[i] === "E" ||
+      newArr[i] === "I" ||
+      newArr[i] === "O" ||
+      newArr[i] === "U"
+    ) {
+      num += 1;
     }
-    return num;
+  }
+  return num;
 };
 console.log(numVowels("orange"));
 console.log(numVowels("lighthouse labs"));
