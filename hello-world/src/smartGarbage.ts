a99253a61bb36dba6f01d61ee9c8e15a7eb5607f
Richard Au <au.richard4@gmail.com>
2022-12-17 12:02:15 -0800

    Fixing code.

--- hello-world/src/smartGarbage.ts
+++ hello-world/src/smartGarbage.ts
@@ -1,15 +1,15 @@
-let bins: {
-  waste: number,
-  recycling: number,
-  compost: number;
-} = {
-  waste: 0,
-  recycling: 0,
-  compost: 0
-};
-const smartGarbage = function (trash: string, bins: object) {
-  if (trash === Object.keys(bins))
-};
+// let bins: {
+//   waste: number,
+//   recycling: number,
+//   compost: number;
+// } = {
+//   waste: 0,
+//   recycling: 0,
+//   compost: 0
+// };
+// const smartGarbage = function (trash: string, bins: object) {
+//   if (trash === Object.keys(bins))
+// };
 
 let bins = {
   waste: 0,
