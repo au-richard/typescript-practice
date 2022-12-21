d1aa22bfcadf84e2e5f0e0b50b9f86632ce79010
Richard Au <au.richard4@gmail.com>
2022-12-20 19:49:32 -0800

    Fixing code.

--- hello-world/distr/sumLargestNum.js
+++ hello-world/distr/sumLargestNum.js
@@ -1,15 +1,14 @@
 "use strict";
 const sumLargestNum = function (data) {
-    let num = data[0];
-    if (data.length < 2) {
-        return "Array length must be at least 2";
+  let num = data[0];
+  if (data.length < 2) {
+    return "Array length must be at least 2";
+  } else {
+    for (let i = 1; i < data.length; i++) {
+      num += data[i];
     }
-    else {
-        for (let i = 1; i < data.length; i++) {
-            num += data[i];
-        }
-        return `The answer is ${num}.`;
-    }
+    return `The answer is ${num}.`;
+  }
 };
 console.log(sumLargestNum([10]));
 console.log(sumLargestNum([1, 10]));
