5de2133b304ee4755ead2e4999a80d87c6776ef3
Richard Au <au.richard4@gmail.com>
2022-12-22 19:44:56 -0800

    Fixing index.ts.

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -105,13 +105,13 @@ const lastIndexOf = function (numArr: number[], num: number): number {
   for (let i = numArr.length - 1; i >= -1; i--) {
     if (numArr[i] === num) {
       return i;
+
     }
-  }
-  return -1;
-};
-console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
-console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
-console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
-console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
-console.log(lastIndexOf([], 3), "=?", -1);
-console.log(lastIndexOf([3], 3), "=?", 0);
+    return -1;
+  };
+  console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+  console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+  console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+  console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+  console.log(lastIndexOf([], 3), "=?", -1);
+  console.log(lastIndexOf([3], 3), "=?", 0);
